669f8c393cdd3fc7bc0166293e053a6f7ec51c44
igouProto <48474345+igouProto@users.noreply.github.com>
2023-07-03 12:01:53 -0700

    fix 11 fullscreen and more notes

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -82,14 +82,12 @@ const scrub = (e) => {
 
 // fullscreen!
 const toggleFullscreen = () => {
-    if (video.requestFullscreen) {
-        video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) { /* Firefox */
-        video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-        video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) { /* IE/Edge */
-        video.msRequestFullscreen();
+    if (player.requestFullscreen) {
+        player.requestFullscreen();
+    }
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
     }
 }
 
